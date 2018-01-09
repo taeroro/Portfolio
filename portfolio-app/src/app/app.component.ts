632b9ec444d847850b9b8948a4a65fd89f3c8fa3
Ryan Fan <fanzuyuan1996@gmail.com>
2018-01-09 13:37:28 -0500

    Scroll changes

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   logoPath = '../assets/portfolio_1.png';
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  // handle scroll event
+  scroll = (): void => {
+    var logo = document.querySelector(".logoImg");
+    // when scrolled pass first container
+    if (window.scrollY >= window.innerHeight - 100) {
+      if (logo.className == "logoImg") logo.className += " changeFilter";
+    }
+    // when scrolled back to first container
+    else if (window.scrollY < window.innerHeight - 100) {
+      if (logo.className == "logoImg changeFilter") logo.className = "logoImg";
+    }
+  };
 }
